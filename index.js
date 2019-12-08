906ee78abd1573964a1178cd4c8a1e2f0c21909b
Dominic Chu <dominic.m.chu@gmail.com>
2019-12-08 11:47:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+let main_thing = document.querySelector("main#main");
+main_thing.remove(); 
+
+
+
+
+let body_1 = document.querySelector("body")
+
+body_1.innerHTML = `<h1 id="victory">YOUR-NAME is the champion</h1>`
+
+let newHeader = document.querySelector("h1#victory")
